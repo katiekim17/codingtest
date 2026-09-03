5bc3875c14d9d1baf98962a62bb69a1155cb4fb6
katiekim17 <pyeongsuk.kim88@gmail.com>
2026-09-03 22:05:52 +0900

    solve: 프로그래머스 시저 암호 (12926)

--- programmers/12926.py
+++ programmers/12926.py
@@ -1,13 +1,20 @@
 # 프로그래머스 - 시저 암호
 # https://school.programmers.co.kr/learn/courses/30/lessons/12926
 
+import string
 def solution(s, n):
-    result = []
-    for c in s:
-        if c == " ":
-            result.append(" ")
-        elif c.isupper():
-            result.append(chr((ord(c) - ord("A") + n) % 26 + ord("A")))
-        else:
-            result.append(chr((ord(c) - ord("a") + n) % 26 + ord("a")))
-    return "".join(result)
+    answer = ''
+    lower = string.ascii_lowercase + string.ascii_lowercase
+    upper = string.ascii_uppercase + string.ascii_uppercase
+    
+    for char in s: # 한 글자씩 검사하기
+        if char == ' ':
+            answer += ' '
+        elif char.isupper():
+            start_idx = upper.index(char)
+            answer += upper[start_idx + n]
+        elif char.islower():
+            start_idx = lower.index(char)
+            answer += lower[start_idx + n]
+
+    return answer
